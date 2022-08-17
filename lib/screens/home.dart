0ca3387b2e144cc0bfa07ecd6329066b56793662
Jesus Redondo <github@jesusredondo.com>
2022-08-17 10:04:51 +0200

    Fix Navigator

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../services/trip.service.dart';
+import '../services/trip.service.dart' as TripService;
+import '../bloc/blocs.dart';
+import '../models/trip.model.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -19,9 +22,16 @@ class HomePageView extends StatelessWidget {
     return Center(
         child: ElevatedButton(
       child: Text('Load Trips'),
-      onPressed: () {
+      onPressed: () async {
+        await loadTrips(context);
         Navigator.pushNamed(context, 'trips');
       },
     ));
+  }
+
+  loadTrips(context) async {
+    var response = await TripService.getTrips();
+    final tripBloc = BlocProvider.of<TripBloc>(context, listen: false);
+    tripBloc.add(UpdateTrips(tripsFromString(response)));
   }
 }
