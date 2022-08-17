1397fc1369f9a184d8e57d97ce1777edd37f33fa
Jesus Redondo <github@jesusredondo.com>
2022-08-17 10:03:29 +0200

    Fix model toString and fromString

--- lib/models/trip.model.dart
+++ lib/models/trip.model.dart
@@ -2,8 +2,10 @@ import 'dart:convert';
 
 import 'package:equatable/equatable.dart';
 
-List<Trip> tripsFromJson(String str) => List<Trip>.from(json.decode(str).map((x) => Trip.fromJson(x)));
-String tripsToJson(List<Trip> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+List<Trip> tripsFromString(String str) => List<Trip>.from(json.decode(str).map((x) {
+      return Trip.fromJson(x);
+    }));
+String tripsToString(List<Trip> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class Trip extends Equatable {
   String name;
@@ -16,7 +18,10 @@ class Trip extends Equatable {
   @override
   bool get stringify => true;
 
-  factory Trip.fromJson(Map<String, dynamic> json) => Trip(name: json["name"], startDate: DateTime.parse(json["startDate"]));
+  factory Trip.fromJson(Map<String, dynamic> json) {
+    DateTime time = DateTime.parse(json["startDate"]);
+    return Trip(name: json["name"], startDate: time);
+  }
   // DateTime.parse('1969-07-20 20:18:04Z'); // 8:18pm
   Map<String, dynamic> toJson() => {'name': name, 'startDate': startDate.toIso8601String()};
 }
