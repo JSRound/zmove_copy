069683b36d5d44ac515585cec3649d858e0d335a
Jesus Redondo <github@jesusredondo.com>
2022-08-17 10:04:31 +0200

    Fix trip service

--- lib/services/trip.service.dart
+++ lib/services/trip.service.dart
@@ -4,7 +4,7 @@ Future<String> getTrips() {
   return Future.delayed(Duration(seconds: 1), () {
     String time = DateTime.now().add(const Duration(minutes: 1)).toIso8601String();
     Object tripsJson = [
-      {"name": "Trip 1", "date": time}
+      {"name": "Trip 1", "startDate": time}
     ];
     String tripsString = json.encode(tripsJson);
     return tripsString;
