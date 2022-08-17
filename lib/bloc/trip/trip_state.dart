73b2897302dc8e69a32e2321107dcb03f14124af
Jesus Redondo <github@jesusredondo.com>
2022-08-17 10:04:18 +0200

    Fix TripState

--- lib/bloc/trip/trip_state.dart
+++ lib/bloc/trip/trip_state.dart
@@ -1,6 +1,8 @@
 part of 'trip_bloc.dart';
 
 class TripState {
-  String title;
-  TripState({required this.title});
+  List<Trip> trips;
+  TripState({required this.trips});
+
+  TripState copyWith({List<Trip>? trips}) => TripState(trips: trips ?? this.trips);
 }
