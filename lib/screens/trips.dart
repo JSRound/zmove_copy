cb1d28b20c491f851f218cc555aafa1a227d78f6
Jesus Redondo <github@jesusredondo.com>
2022-08-17 10:05:08 +0200

    Implements BlocBuilder

--- lib/screens/trips.dart
+++ lib/screens/trips.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/blocs.dart';
+import '../models/trip.model.dart';
 
 class TripsPage extends StatelessWidget {
   const TripsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text('hola que ase'));
+    return Scaffold(appBar: AppBar(title: Text('My Trips')), body: TripsPageView());
+  }
+}
+
+class TripsPageView extends StatelessWidget {
+  const TripsPageView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<TripBloc, TripState>(builder: (context, state) {
+      return Column(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center, children: [
+        for (Trip item in state.trips) TripCard(trip: item),
+      ]);
+    });
+  }
+}
+
+class TripCard extends StatelessWidget {
+  late Trip trip;
+  TripCard({Key? key, required Trip trip}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center, children: [
+      Text(trip.name),
+      Text('${trip.startDate}'),
+    ]));
   }
 }
